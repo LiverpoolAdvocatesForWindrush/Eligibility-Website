cc5e3de4c6ed6826450b8ead507ed72e3e4463c2
lydiaankers <lydiaankers14@gmail.com>
2025-02-14 15:28:28 +0000

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { StrictMode } from 'react';
 //import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-
     <StrictMode>
         <App />
     </StrictMode>
