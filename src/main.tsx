3f4dba1751c8947c5a1fe414585fb67c8feea2eb
lydiaankers <lydiaankers14@gmail.com>
2025-02-14 14:59:49 +0000

    reload fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+/*import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';*/
 import App from './App.tsx';
 import './index.css';
-//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
+/*createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <HashRouter>
             <App />
         </HashRouter>
     </StrictMode>
-);
-
-/*ReactDOM.render(
-    <StrictMode>
+);*/
+ReactDOM.render(
+    <React.StrictMode>
         <HashRouter>
             <App />
         </HashRouter>
-    </StrictMode>,
+    </React.StrictMode>,
     document.getElementById('root')
-);*/
+);
+
