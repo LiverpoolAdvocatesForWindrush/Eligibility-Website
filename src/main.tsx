46777ce8439da5f3785a6455938595d1b2729eb6
lydiaankers <lydiaankers14@gmail.com>
2025-03-05 12:35:08 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+/*import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';*/
 import App from './App.tsx';
 import './index.css';
 
 
-createRoot(document.getElementById('root')!).render(
+/*createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
     </StrictMode>
-);
+);*/
 
+
+import { HashRouter as Router } from "react-router-dom";
+
+<Router>
+    <App />
+</Router>
 
 /*ReactDOM.render(
     <React.StrictMode>
